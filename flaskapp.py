b9e13cbcf7148fef22631d8fa11d5163874defec
Rohan Somkuwar <100048559+rohan-somkuwar@users.noreply.github.com>
2023-04-02 00:55:40 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
 def index():
-    name = request.args.get('name', 'world')
-    return f'<h1>Hell, {name}!</h1>\n<p>Welcome to the website.</p>'
+    name = request.args.get('name', 'Website')
+    return f'<h1>Rohan, {name}!</h1>\n<p>Thankyou for visit to the website.</p>'
 
 app.run(host='localhost', port=8000)
